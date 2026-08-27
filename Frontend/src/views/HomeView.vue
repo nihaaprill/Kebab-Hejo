75579daa94d3d99500766aac09cc85970ab4e4c7
kelapasawit <nihaaapril@gmail.com>
2026-08-28 00:41:08 +0700

    Add files via upload

---
+++ Frontend/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+import HeroSection from '@/components/HeroSection.vue'
+import AboutSection from '@/components/AboutSection.vue'
+import MenuSection from '@/components/MenuSection.vue'
+import FranchiseSection from '@/components/FranchiseSection.vue'
+import CoverageSection from '@/components/CoverageSection.vue'
+import GallerySection from '@/components/GallerySection.vue'
+import Footer from '@/components/Footer.vue'
+import WhatsAppButton from '@/components/WhatsAppButton.vue'
+</script>
+
+<template>
+  <main class="min-h-screen overflow-x-hidden">
+    <Navbar />
+    <HeroSection />
+    <AboutSection />
+    <MenuSection />
+    <FranchiseSection />
+    <CoverageSection />
+    <TestimonialSection />
+    <GallerySection />
+    <Footer />
+    <WhatsAppButton />
+  </main>
+</template>
